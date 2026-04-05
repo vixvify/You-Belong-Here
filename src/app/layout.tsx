abd305f303ed62c876f3c29d688fbfd334b2c181
vixvify <asnaweeezor@gmail.com>
2026-04-05 14:59:12 +0700

    refactor: update font usage in layout and improve placeholder text in Custom2 component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,12 @@
 "use client";
 
-import { Kanit } from "next/font/google";
+import { Sarabun } from "next/font/google";
 import "./globals.css";
 import TransitionOverlay from "@/components/transition/Transition";
 
-const kanit = Kanit({
-  subsets: ["thai", "latin"],
-  weight: ["400", "600"],
-  variable: "--font-kanit",
+const sarabun = Sarabun({
+  subsets: ["latin", "thai"],
+  weight: ["300", "400", "700"],
 });
 
 export default function RootLayout({
@@ -18,7 +17,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={` ${kanit.className} bg-black antialiased flex justify-center`}
+        className={` ${sarabun.className} bg-black antialiased flex justify-center`}
       >
         <div className="w-full max-w-120 min-h-screen bg-white relative overflow-hidden">
           {children}
